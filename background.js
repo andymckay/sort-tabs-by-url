7fc46c2191e672284b549590e113f7e184923886
Andy McKay <amckay@mozilla.com>
2016-03-15 10:33:19 +0100

    use commands

--- background.js
+++ background.js
@@ -1,19 +1,27 @@
-'use strict';
-/*global chrome:false */
-
 function sortTabs(a, b) {
   return (a.url.split('//')[1] || a.url) > (b.url.split('//')[1] || b.url);
 }
 
-chrome.browserAction.onClicked.addListener(function(aTab) {
-  chrome.tabs.query(
-    {
-      windowId: chrome.windows.WINDOW_ID_CURRENT,
-      pinned: false,
-    },
-    function(tabs){
-      tabs.sort(sortTabs);
-      chrome.tabs.move(tabs.map(function(i) { return i.id}, 1), {index: 1});
+function sort() {
+  chrome.tabs.query({pinned: true},
+    function(pinned) {
+      chrome.tabs.query({pinned: false},
+        function(tabs) {
+          tabs.sort(sortTabs);
+          chrome.tabs.move(
+            tabs.map(function(i) { return i.id;}),
+            {windowId: 0, index: pinned.length}
+          );
+        }
+      );
     }
-  )
+  );
+}
+
+chrome.browserAction.onClicked.addListener(function(aTab) {
+  sort();
+});
+
+chrome.commands.onCommand.addListener(function(command) {
+  sort();
 });
